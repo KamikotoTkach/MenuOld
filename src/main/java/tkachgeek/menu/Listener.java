d524b14a7c4f7bc37cc20601255d0c2c315fda0a
Soul_KRT <soul.krt.00@gmail.com>
2023-02-07 15:12:55 +0300

    many fix

--- src/main/java/tkachgeek/menu/Listener.java
+++ src/main/java/tkachgeek/menu/Listener.java
@@ -1,6 +1,5 @@
 package tkachgeek.menu;
 
-import io.papermc.paper.event.player.AsyncChatEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,45 +11,46 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import java.util.WeakHashMap;
 
 public class Listener implements org.bukkit.event.Listener {
-  static WeakHashMap<Player, Menu> toTakeValue = new WeakHashMap<>();
+   static WeakHashMap<Player, Menu> toTakeValue = new WeakHashMap<>();
 
-  public static void getNextMessage(Player player, Menu menuInterface) {
-    toTakeValue.put(player, menuInterface);
-  }
-  @EventHandler
-  void onInventoryClick(InventoryClickEvent event) {
-    if (Menus.isInitialized(event.getInventory())) {
-      if (event.getClickedInventory() != null && !event.getClickedInventory().equals(event.getWhoClicked().getInventory())) {
-        Menus.handleInventoryClick(event);
-      } else {
-        Menus.handleOwnInventoryClick(event);
+   public static void getNextMessage(Player player, Menu menuInterface) {
+      toTakeValue.put(player, menuInterface);
+   }
+
+   @EventHandler
+   void onInventoryClick(InventoryClickEvent event) {
+      if (Menus.isInitialized(event.getInventory())) {
+         if (event.getClickedInventory() != null && !event.getClickedInventory().equals(event.getWhoClicked().getInventory())) {
+            Menus.handleInventoryClick(event);
+         } else {
+            Menus.handleOwnInventoryClick(event);
+         }
       }
-    }
-  }
+   }
 
-  @EventHandler
-  void onInventoryClose(InventoryCloseEvent event) {
-    if (Menus.isInitialized(event.getInventory())) {
-      Menus.handleInventoryClose(event);
-    }
-  }
-
-  @EventHandler
-  void onInventoryDrag(InventoryDragEvent event) {
-    if (Menus.isInitialized(event.getInventory())) {
-      Menus.handleInventoryDrag(event);
-    }
-  }
-
-  @EventHandler
-  void onChatMessage(AsyncPlayerChatEvent event) {
-    Bukkit.getScheduler().scheduleSyncDelayedTask(Menus.plugin, () -> {
-      if (toTakeValue.containsKey(event.getPlayer())) {
-        toTakeValue.get(event.getPlayer()).returnValue(event.getMessage()); //todo:мб тут багуля, поменял эвент и метод, но не чекал
-        toTakeValue.remove(event.getPlayer());
+   @EventHandler
+   void onInventoryClose(InventoryCloseEvent event) {
+      if (Menus.isInitialized(event.getInventory())) {
+         Menus.handleInventoryClose(event);
       }
-    }, 0);
-    if (toTakeValue.containsKey(event.getPlayer())) event.setCancelled(true);
+   }
 
-  }
+   @EventHandler
+   void onInventoryDrag(InventoryDragEvent event) {
+      if (Menus.isInitialized(event.getInventory())) {
+         Menus.handleInventoryDrag(event);
+      }
+   }
+
+   @EventHandler
+   void onChatMessage(AsyncPlayerChatEvent event) {
+      Bukkit.getScheduler().scheduleSyncDelayedTask(Menus.plugin, () -> {
+         if (toTakeValue.containsKey(event.getPlayer())) {
+            toTakeValue.get(event.getPlayer()).returnValue(event.getMessage()); //todo:мб тут багуля, поменял эвент и метод, но не чекал
+            toTakeValue.remove(event.getPlayer());
+         }
+      }, 0);
+      if (toTakeValue.containsKey(event.getPlayer())) event.setCancelled(true);
+
+   }
 }
